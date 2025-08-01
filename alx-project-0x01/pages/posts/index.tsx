98516624327e41e60791fc02d3e8c61bb724818e
Hanan <your-email@example.com>
2025-08-01 05:42:00 -0700

    feat: set up navigation between pages using next/link

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,17 @@
-const PostPage = () => {
-    return <div className="p-4">Posts Page</div>;
+// pages/posts/index.tsx
+import Header from "../../components/layout/Header"; // for posts and users pages
+
+
+const PostsPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Posts Page</h1>
+        {/* Posts will go here */}
+      </main>
+    </div>
+  );
 };
 
-
-export default PostPage;
+export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,15 @@
-const UserPage = () => {
-    return <div className="p-4">Users Page</div>
+import Header from "../../components/layout/Header"; // for posts and users pages
+
+const UsersPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Users Page</h1>
+        {/* Users will go here */}
+      </main>
+    </div>
+  );
 };
 
-export default UserPage;
+export default UsersPage;

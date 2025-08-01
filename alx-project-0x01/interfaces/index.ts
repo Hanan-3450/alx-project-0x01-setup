1bdf89a5b8f62102977a15c2eed91654377476d2
Hanan <your-email@example.com>
2025-08-01 06:41:27 -0700

    fix: fully type UserCard component using UserProps interface

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,12 +1,4 @@
-export interface PostProps {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
 
-
-// interfaces/index.ts
 
 export interface UserProps {
   id: number;
